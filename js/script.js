aa1e02f6969452d641ee870bc42669abf60a2777
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2024-12-28 08:22:09 +0000

    more

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@ const resumeBtns = document.querySelectorAll('.resume-btn');
 
 resumeBtns.forEach((btn, idx) => {
   btn.addEventListener('click', () => {
+    const resumeDetails = document.querySelectorAll('.resume-detail');
+
     resumeBtns.forEach(btn => {
       btn.classList.remove('active');
     });
     btn.classList.add('active');
+
+    resumeDetails.forEach(detail => {
+      detail.classList.remove('active');
+    });
+    resumeDetails[idx].classList.add('active');
   });
 });
