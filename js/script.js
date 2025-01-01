f46803892917530a0b185d2c63f01e2d5b535be1
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2025-01-01 13:00:01 +0000

    hjdbjbsd

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 const navLinks = document.querySelectorAll('header nav a');
 const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
 
 const activePage = () => {
+  const header = document.querySelector('header');
+  const barsBox = document.querySelector('.bars-box');
+
+  header.classList.remove('active');
+  setTimeout(() => {
+    header.classList.add('active');
+  }, 1000);
+
   navLinks.forEach((link) => {
     link.classList.remove('active');
+  });
+
+  barsBox.classList.remove('active');
+  setTimeout(() => {
+    barsBox.classList.add('active');
+  }, 1000);
+
+  sections.forEach((section) => {
+    section.classList.remove('active');
   });
 }
 
@@ -13,6 +31,10 @@ navLinks.forEach((link, idx) => {
       activePage();
 
       link.classList.add('active');
+
+      setTimeout(() => {
+        sections[idx].classList.add('active');
+      }, 1000);
     }
   });
 });
@@ -22,6 +44,10 @@ logoLink.addEventListener('click', () => {
     activePage();
 
     navLinks[0].classList.add('active');
+
+    setTimeout(() => {
+      sections[0].classList.add('active');
+    }, 1000);
   }
 });
 
