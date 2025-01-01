e03880981b61e5a10a237df06b4469be73c1e119
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2025-01-01 11:41:55 +0000

    added a typo to my home logo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+const navLinks = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
+
+const activePage = () => {
+  navLinks.forEach((link) => {
+    link.classList.remove('active');
+  });
+}
+
+navLinks.forEach((link, idx) => {
+  link.addEventListener('click', () => {
+    if (!link.classList.contains('active')) {
+      activePage();
+
+      link.classList.add('active');
+    }
+  });
+});
+
+logoLink.addEventListener('click', () => {
+  if (!navLinks[0].classList.contains('active')) {
+    activePage();
+
+    navLinks[0].classList.add('active');
+  }
+});
+
 const resumeBtns = document.querySelectorAll('.resume-btn');
 
 resumeBtns.forEach((btn, idx) => {
